65718c078360c1df2e749fcda12c2ce7b2be60f1
vigneshraj venkatesh <vigneshrajvenkatesh@vigneshrajs-MacBook-Pro.local>
2021-11-19 22:48:34 +0530

    fully working game, can be played multiple times

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,28 @@
 # viggy's Fizz Buzz Game
+prompt_if_game_over = True
+while prompt_if_game_over:
+    prompt_if_number = True
 
+    while prompt_if_number:
+        Number_goes_upto = input("Enter the number up to which you would like to play the FIZZBUZZ Game\n")
+        if Number_goes_upto.isdigit():
+            prompt_if_number = False
+        else:
+            print("Error: Kindly  entered a valid number (eg 100)")
+
+
+    for number in range(1,int(Number_goes_upto)+1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+    play_again = input("1) Enter 'y' if you want to play the game again\n2) Enter any other character to exit\n").upper()
+    if play_again != "Y":
+        prompt_if_game_over = False
+
+
+print("Hope you had a wonderfull experice see you back soon, bye")
