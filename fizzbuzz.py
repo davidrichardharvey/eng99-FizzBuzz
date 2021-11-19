e3a0dc4a5fb7da115b9ef145fe12220145b6cb80
schowdhury2019 <sunnychowdhury@hotmail.com>
2021-11-19 17:01:05 +0000

    Fixed bug where nothing happens when input is 0, any input greater than 1000 defaults to 1000 to avoid processing large numbers

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,11 @@
 num = None
+default = 1000
+
 while True:
     num = input("Please enter a valid number: ")
-    if num.isdigit():
-        num = 1000 if int(num) > 1000 else int(num)
+    if num.isdigit() and num != "0":
+        num = default if int(num) > default else int(num)
         break
-
 
 for i in range(1, num + 1):
     if i % 3 == 0 and i % 5 == 0:
@@ -15,3 +16,5 @@ for i in range(1, num + 1):
         print("Fizz")
     else:
         print(i)
+
+
