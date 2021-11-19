df7c32ffe0fbadeedf4526ebe3d3ea26d202dbfe
schowdhury2019 <sunnychowdhury@hotmail.com>
2021-11-19 17:06:20 +0000

    Replaced the True condition of while loop with is_valid variable containing a boolean value

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,12 @@
 num = None
+is_valid = False
 default = 1000
 
-while True:
+while not is_valid:
     num = input("Please enter a valid number: ")
     if num.isdigit() and num != "0":
         num = default if int(num) > default else int(num)
-        break
+        is_valid = True
 
 for i in range(1, num + 1):
     if i % 3 == 0 and i % 5 == 0:
