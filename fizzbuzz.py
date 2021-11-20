d90f37dfcad71a3bccdf7c7f34e0a1db85cdcacd
shaeed <shaeednoordeen@gmail.com>
2021-11-20 18:44:42 +0200

    cleaned code and added option for lowercase y

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,10 +2,10 @@ play = True
 print('Welcome to FizzBuzz!')
 while play:
     counter = 1
-    number = (input('Please enter a number (1-100): '))
+    number = int(input('Please enter a number (1-100): '))
 
-    if int(number) in range(1,101):
-        while (counter < int(number)):
+    if number in range(1,101):
+        while (counter < number):
             if counter % 3 == 0 & counter % 5 == 0:
                 print('FizzBuzz')
             elif counter % 3 == 0:
@@ -22,6 +22,8 @@ while play:
     replay = str(input('Do you want to play again? (Y/N)'))
     if replay == 'N' or replay == 'n':
         play = False
+    elif replay == 'Y' or 'y':
+        play = True
 print('Thanks for playing!')
 
 
