a45b5735f3c7a5ec4a1c6663434342f6de656ccf
shaeed <shaeednoordeen@gmail.com>
2021-11-20 17:47:08 +0200

    added play again feature

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,26 @@
-number = int(input('Please enter a maximum number: '))
-counter = 1
+play = True
 
-while (counter < number):
-    if counter % 3 == 0 & counter % 5 == 0:
-        print('FizzBuzz')
-    elif counter % 3 == 0:
-        print(('Fizz'))
-    elif counter % 5 == 0:
-        print('Buzz')
-    else:
-        print(counter)
-    counter += 1
+while play:
+    counter = 1
+    number = int(input('Please enter a maximum number: '))
+
+
+    while (counter < number):
+        if counter % 3 == 0 & counter % 5 == 0:
+            print('FizzBuzz')
+        elif counter % 3 == 0:
+            print(('Fizz'))
+        elif counter % 5 == 0:
+            print('Buzz')
+        else:
+            print(counter)
+        counter += 1
+
+    counter = 1
+    replay = str(input('Do you want to play again? (Y/N)'))
+    if replay == 'N':
+        play = False
+
 
 
 
