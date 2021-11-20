5276acfda20212cdc10f06a81bda37caaaae102d
shaeed <shaeednoordeen@gmail.com>
2021-11-20 18:42:22 +0200

    Added welcome message, thanks for playing message and range limit of 1-100.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,25 +1,28 @@
 play = True
-
+print('Welcome to FizzBuzz!')
 while play:
     counter = 1
-    number = int(input('Please enter a maximum number: '))
+    number = (input('Please enter a number (1-100): '))
 
-
-    while (counter < number):
-        if counter % 3 == 0 & counter % 5 == 0:
-            print('FizzBuzz')
-        elif counter % 3 == 0:
-            print(('Fizz'))
-        elif counter % 5 == 0:
-            print('Buzz')
-        else:
-            print(counter)
-        counter += 1
+    if int(number) in range(1,101):
+        while (counter < int(number)):
+            if counter % 3 == 0 & counter % 5 == 0:
+                print('FizzBuzz')
+            elif counter % 3 == 0:
+                print(('Fizz'))
+            elif counter % 5 == 0:
+                print('Buzz')
+            else:
+                print(counter)
+            counter += 1
+    else:
+        print('Please enter correct value')
 
     counter = 1
     replay = str(input('Do you want to play again? (Y/N)'))
-    if replay == 'N':
+    if replay == 'N' or replay == 'n':
         play = False
+print('Thanks for playing!')
 
 
 
