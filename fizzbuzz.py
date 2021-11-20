c24a93ce3d9d75f20f02954065650bb172efbda9
vigneshraj venkatesh <vigneshrajvenkatesh@vigneshrajs-MacBook-Pro.local>
2021-11-20 16:02:41 +0530

    same code after eliminating pep8 warnings

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,16 @@
-# viggy's Fizz Buzz Game
+# Viggy's Fizz Buzz Game
 prompt_if_game_over = True
 while prompt_if_game_over:
     prompt_if_number = True
 
     while prompt_if_number:
-        Number_goes_upto = input("Enter the number up to which you would like to play the FIZZBUZZ Game\n")
-        if Number_goes_upto.isdigit():
+        Number_Limit = input("Enter the number up to which you would like to play the FIZZBUZZ Game\n")
+        if Number_Limit.isdigit():
             prompt_if_number = False
         else:
             print("Error: Kindly  entered a valid number (eg 100)")
-
-
-    for number in range(1,int(Number_goes_upto)+1):
+    print("The Numbers will start print:")
+    for number in range(1, int(Number_Limit) + 1):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz")
         elif number % 3 == 0:
@@ -20,7 +19,7 @@ while prompt_if_game_over:
             print("Buzz")
         else:
             print(number)
-    play_again = input("1) Enter 'y' if you want to play the game again\n2) Enter any other character to exit\n").upper()
+    play_again = input("1) Enter 'y' if you want to play the game again\n2)Enter any other character to exit\n").upper()
     if play_again != "Y":
         prompt_if_game_over = False
 
