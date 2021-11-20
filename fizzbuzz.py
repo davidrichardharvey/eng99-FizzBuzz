df1ccac83fd2bca6fec70498000c591f7ebb68f8
Rokas <rvisockis@spartaglobal.com>
2021-11-20 17:11:09 +0000

    Did final changes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,10 @@
 start_game = True
 
 while start_game:
-
-    up_to_number = input("Pick a number you will count to?\n")
-
+    up_to_number = input("Pick a number you want to count up to?\n")
 
     if int(up_to_number) in range(1, 101):
-        for i in range (1, int(up_to_number)):
+        for i in range(1, int(up_to_number)):
             if i % 3 == 0 and i % 5 == 0:
                 print("BuzzFizz")
             elif i % 3 == 0:
@@ -17,8 +15,9 @@ while start_game:
                 print(i)
     else:
         print("Enter a valid number that is between 1 - 100")
+        start_game = True
 
-    start_over = input("Thank you for playing, type Y if you want to play again\n")
+    start_over = input("Thank you for playing, type Y if you want to play again or any other key to stop\n")
     start_over = start_over.upper()
 
     if start_over == "Y":
@@ -26,5 +25,3 @@ while start_game:
     else:
         print("Thank you for playing!")
         break
-
-
