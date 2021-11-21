7ce37bd32efef810b127293e08b3b1830cffa7bc
Delwar Hussain <Dilwar1998@hotmail.co.uk>
2021-11-21 14:23:49 +0000

    Fizzbuzz 1 to x complete

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,6 +2,9 @@
 
 n = 100
 i = 1
+
+print("Fizzbuzz 1 to 100")
+print("----START----")
 while i <= n:
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
@@ -13,3 +16,33 @@ while i <= n:
         print(i)
     i += 1
 
+print("----END----\n")
+
+
+# Fizzbuzz 1 up to x
+
+prompt_for_number = True
+x = None
+while prompt_for_number:
+    tempx = input("What number would you like the FizzBuzz to go up to: ")
+    if tempx.isdigit():
+        prompt_for_number = False
+        x = int(tempx)
+    else:
+        print("please enter number  in digits")
+
+
+counter = 1
+
+print("Starting FizzBuss \n-----------------")
+while counter <= x:
+    if counter % 3 == 0 and counter % 5 == 0:
+        print("FizzBuzz")
+    elif counter % 3 == 0:
+        print("Fizz")
+    elif counter % 5 == 0:
+        print("Buzz")
+    else:
+        print(counter)
+    counter += 1
+print("----------------- \nFizzBuzz Ended")
