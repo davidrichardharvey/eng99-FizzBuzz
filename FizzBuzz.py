c89a3434ee9dd3d99630de6ae26cccb4e96cd670
schowdhury2019 <sunnychowdhury@hotmail.com>
2021-11-23 13:39:55 +0000

    Cleaned up code a little more and fixed multi 0 bug

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,21 +1,19 @@
 class FizzBuzz:
-    def __init__(self, default = 1000, is_valid = False):
-        self.default = default
-        self.is_valid = is_valid
-
+    def __init__(self, default=1000, is_valid=False):
+        self._default = default
+        self._is_valid = is_valid
 
     def is_valid_number(self, num):
-        if num.isdigit() and num != "0":
-            num = self.default if int(num) > self.default else int(num)
-            self.is_valid = True
+        if num.isdigit() and num != int(num) > 0:
+            self._is_valid = True
 
     def fizz_buzz(self):
         num = None
-        while not self.is_valid:
+        while not self._is_valid:
             num = input("Please enter a valid number: ")
             self.is_valid_number(num)
 
-        num = int(num) if int(num) < self.default else self.default
+        num = self._default if int(num) > self._default else int(num)
 
         for i in range(1, num + 1):
             if i % 3 == 0 and i % 5 == 0:
