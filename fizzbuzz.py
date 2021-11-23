881ac62fc909e65774a669280eb42484bada1553
Ivan Kalashnikov <ivan@kalashnikov.uk>
2021-11-23 13:22:59 +0000

    refactored into a function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,15 +4,18 @@
 # - If the number is divisible by both, print FizzBuzz instead
 # - Start the game by asking the user to provide the "up to" number
 
-num = 0
-limit = int(input("Enter maximum boundary \n"))
-while num <= limit - 1:
-    num += 1
-    if num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
-    elif num % 3 == 0:
-        print("Fizz")
-    elif num % 5 == 0:
-        print("Buzz")
-    else:
-        print(num)
+
+def fizz_buzz(limit):
+    num = 0
+    while num <= limit - 1:
+        num += 1
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+print(fizz_buzz(int(input("Enter maximum boundary \n"))))
