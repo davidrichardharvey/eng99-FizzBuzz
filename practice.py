31487519664a6b2db18d559511e7ae86239c18f8
schowdhury2019 <sunnychowdhury@hotmail.com>
2021-11-23 10:17:06 +0000

    Moved the functionality into a class called Solution

---
+++ practice.py
@@ -0,0 +1,7 @@
+def message(func, message):
+    return func(message)
+
+
+
+hello = message("This is a decorated")
+
---
+++ solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def __init__(self, default = 1000, is_valid = False):
+        self.default = default
+        self.is_valid = is_valid
+
+
+    def is_valid_number(self, num):
+        if num.isdigit() and num != "0":
+            num = self.default if int(num) > self.default else int(num)
+            self.is_valid = True
+
+    def fizz_buzz(self):
+        num = None
+        while not self.is_valid:
+            num = input("Please enter a valid number: ")
+            self.is_valid_number(num)
+
+        for i in range(1, int(num) + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz")
+            elif i % 5 == 0:
+                print("Buzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            else:
+                print(i)
