0de1770db463248be68ff7a4dd34b097f1b53293
AM <83598489+a-miah@users.noreply.github.com>
2021-11-24 09:29:15 +0000

    utilised function for fizzbuzz game

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,7 @@
 
 print('Welcome to FizzBuzz!\n')
-start_game = True
 
-while start_game:
-    user_input = input('Please provide a number you would like to play up to: ')
+def fizzbuzz(user_input):
     if user_input.isdigit():
         for i in range(1, (int(user_input) + 1)):
             if i % 3 == 0 and i % 5 == 0:
@@ -14,8 +12,7 @@ while start_game:
                 print('Buzz')
             else:
                 print(i)
-                start_game = False
     else:
         print('Please enter a valid number')
 
-print('\nThanks for playing!')
+print(fizzbuzz(input('Please provide a number you would like to play up to: ')))
